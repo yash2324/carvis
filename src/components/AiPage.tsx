7cfdc73437b6c3c8c6b035131dcbb19e298afc55
Adarsh <adarsh9540984202@gmail.com>
2024-07-23 18:54:30 +0530

    add2

--- src/components/AiPage.tsx
+++ src/components/AiPage.tsx
@@ -1,5 +1,6 @@
 import InputBar from "./InputBar";
 import ImageAvatars from "./Avatar";
+import image from "../../public/image.png"
 function ChatbotInfo() {
   return (
     <div className="relative min-h-screen w-3/4 p-4 pb-16">
@@ -18,7 +19,7 @@ function ChatbotInfo() {
           <p className="text-sm">Engine Type: V6</p>
         </div>
       </div>
-
+      
       <div className="bg-[#D3D3D3] mt-2 flex py-4 border-b-4 rounded-sm">
         <div className="p-2">
           <ImageAvatars src="https://s3-alpha-sig.figma.com/img/b1b8/cf8c/9cab4790e5f3119ce76099d838feb177?Expires=1722816000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=A1zaQcFrMLSpWTrXTZSrPaczn3p7YoQknZfO47NGXpCsOZwb1lZwMSo7vyy3Zd-ozSzlcFBzAeb6u9b5HOeAFZefGGtHkp1v1tHDuBXSezndoUXT5aDGOoG09HaAk5g4OrgFzlcrRXzbk-tcr3lWNRpa890qAITADmrA68SqmDbN2zkbySeluMTMnHySysa9taK7Sw88cCg2FFDupvpqVnq6lbbd-wyTP1TbRGrkg-vJa9hMXRcgg21XX8nOlii-bNPnHIYOg~XRqoylVyhRc1~E7mRf-mxBibTqTinWNqcnkG-tmEr7aIKIOushUdnhFHJ7CQGn2ukRSH5hYe-xMg__" />
@@ -28,8 +29,8 @@ function ChatbotInfo() {
           <p className="text-sm">Diagnostic information will appear here</p>
         </div>
       </div>
-
-      <div className="bg-[#D3D3D3]  mt-4 flex justify-end py-1 border-b-4 ml-20 rounded-sm">
+      
+      <div className="bg-[#D3D3D3] mt-4 flex justify-end py-1 border-b-4 ml-20 rounded-sm relative z-10">
         <div className="flex items-center">
           <div className="text-right mr-2">
             <h3 className="font-semibold text-lg">User</h3>
@@ -40,22 +41,25 @@ function ChatbotInfo() {
           </div>
         </div>
       </div>
-
-      <div className="w-[580px]  h-[250px] mx-20 bg-[#FAFAFA] items-center">
-        <div className="">
-          <img
-            className=" w-[511px] h-[209px] rounded-md"
-            src="https://s3-alpha-sig.figma.com/img/68ae/10da/7622f6f2b43155b98ff59a8f5c9a3919?Expires=1722816000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Wvk9Gpx8Rl8IwryBhgIJeqwHGJj1pSIkn~YUMKhSiVMzkrhuSp3AnH3-1RaOHYF17BkVcY9hP3qvQch~01hnwdTjO9V4tJ73iYa37EX0Yq8gh1I~dnh1l8D4QIr5WYHzqbIguKvlAk8STy1B6n9-5mBFDNLi0s-lIMYCMKrgBElb1RyPn4neEi41Im55t9cTTuNLI9bDscXc~qvQoMBokMkl3X2obzlVaeCNyt6z1hrMsAHtqWNYeI9XdJMOSOdADrvae3bSVB~OziPA9jQk3DpgRUOLpURtEaRsKzUSIpih3Wl7twAcfSPXC-RnI7fEjNgyJK3KRBaliPbmiXmEiA__"
-            alt=""
-          />
-        </div>
-        <div>
-          <h1>Info</h1>
-
-          <p>This means XYZ</p>
-        </div>
-      </div>
-
+      
+      <div className="w-[540px] h-[270px] bg-[#FAFAFA] ml-64 z-20 relative -mt-[25px] flex flex-col justify-start items-start p-4">
+  <div className="relative">
+    <img
+      className="w-[511px] h-[200px] rounded-md"
+      src="https://s3-alpha-sig.figma.com/img/68ae/10da/7622f6f2b43155b98ff59a8f5c9a3919?Expires=1722816000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Wvk9Gpx8Rl8IwryBhgIJeqwHGJj1pSIkn~YUMKhSiVMzkrhuSp3AnH3-1RaOHYF17BkVcY9hP3qvQch~01hnwdTjO9V4tJ73iYa37EX0Yq8gh1I~dnh1l8D4QIr5WYHzqbIguKvlAk8STy1B6n9-5mBFDNLi0s-lIMYCMKrgBElb1RyPn4neEi41Im55t9cTTuNLI9bDscXc~qvQoMBokMkl3X2obzlVaeCNyt6z1hrMsAHtqWNYeI9XdJMOSOdADrvae3bSVB~OziPA9jQk3DpgRUOLpURtEaRsKzUSIpih3Wl7twAcfSPXC-RnI7fEjNgyJK3KRBaliPbmiXmEiA__"
+      alt=""
+    />
+    <img 
+      src={image} 
+      alt="" 
+      className="absolute top-0 right-0 w-8 h-8 cursor-pointer"
+    />
+  </div>
+  <div className="mb-2">
+    <h1 className="font-bold">Info</h1>
+    <p>This means XYZ</p>
+  </div>
+</div>
       <div className="fixed bottom-0 w-3/4 bg-[#D3D3D3] p-4">
         <InputBar />
       </div>
