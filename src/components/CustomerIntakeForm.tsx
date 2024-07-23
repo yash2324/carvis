275808e276aced1abc27548e445ea98f76d315bb
yash2324 <yg232004@gmail.com>
2024-07-23 18:44:14 +0530

    fixed screen 3

--- src/components/CustomerIntakeForm.tsx
+++ src/components/CustomerIntakeForm.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { X } from "lucide-react";
 const CustomerIntakeForm = () => {
   const [customerComplaint, setCustomerComplaint] = useState("");
   const [mechanicNotes, setMechanicNotes] = useState("");
@@ -14,48 +13,121 @@ const CustomerIntakeForm = () => {
   };
 
   return (
-    <div className="bg-black/60 rounded-lg p-8 w-full max-w-md">
+    <div className="bg-[#7F7F7F] border rounded-lg p-8 w-full max-w-md">
       <div className="flex justify-between items-center mb-4">
-        <h2 className="text-xl font-semibold">Customer Intake</h2>
-        <button className="text-white hover:text-gray-900">
-          <X />
+        <h2 className="text-xl font-redHat text-[#030303] font-semibold">
+          Customer Intake
+        </h2>
+        <button className="text-white hover:text-gray-900 bg-">
+          <svg
+            width="28"
+            height="28"
+            viewBox="0 0 28 28"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g filter="url(#filter0_d_5_684)">
+              <path
+                d="M22 0H6C2.68629 0 0 2.68629 0 6V22C0 25.3137 2.68629 28 6 28H22C25.3137 28 28 25.3137 28 22V6C28 2.68629 25.3137 0 22 0Z"
+                fill="white"
+              />
+            </g>
+            <g clip-path="url(#clip0_5_684)">
+              <path
+                d="M18.0834 10.7391L17.2609 9.91663L14.0001 13.1775L10.7392 9.91663L9.91675 10.7391L13.1776 14L9.91675 17.2608L10.7392 18.0833L14.0001 14.8225L17.2609 18.0833L18.0834 17.2608L14.8226 14L18.0834 10.7391Z"
+                fill="black"
+              />
+            </g>
+            <defs>
+              <filter
+                id="filter0_d_5_684"
+                x="0"
+                y="0"
+                width="28"
+                height="28"
+                filterUnits="userSpaceOnUse"
+                color-interpolation-filters="sRGB"
+              >
+                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <feColorMatrix
+                  in="SourceAlpha"
+                  type="matrix"
+                  values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                  result="hardAlpha"
+                />
+                <feOffset />
+                <feComposite in2="hardAlpha" operator="out" />
+                <feColorMatrix
+                  type="matrix"
+                  values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.08 0"
+                />
+                <feBlend
+                  mode="normal"
+                  in2="BackgroundImageFix"
+                  result="effect1_dropShadow_5_684"
+                />
+                <feBlend
+                  mode="normal"
+                  in="SourceGraphic"
+                  in2="effect1_dropShadow_5_684"
+                  result="shape"
+                />
+              </filter>
+              <clipPath id="clip0_5_684">
+                <rect
+                  width="14"
+                  height="14"
+                  fill="white"
+                  transform="translate(7 7)"
+                />
+              </clipPath>
+            </defs>
+          </svg>
         </button>
       </div>
-      <p className="text-white mb-4">Let's gather some basic info</p>
+      <p className="text-[#E1E1E1] mb-4 font-redHat ">
+        Let's gather some basic info
+      </p>
       <div className="mb-4">
-        <label className="block text-white mb-2" htmlFor="customer-complaint">
+        <label
+          className="block text-[#E1E1E1] font-redHat text-[13px] mb-2"
+          htmlFor="customer-complaint"
+        >
           Enter Customer Complaint
         </label>
         <input
           id="customer-complaint"
           type="text"
-          className="w-full p-2  border border-gray-400 rounded-full"
+          className="w-full p-2 placeholder:text-black font-redHat text-[13px] border border-gray-400 rounded-full"
           placeholder="Describe the issues the customer mentioned"
           value={customerComplaint}
           onChange={(e) => setCustomerComplaint(e.target.value)}
         />
       </div>
       <div className="mb-4">
-        <label className="block text-white mb-2" htmlFor="mechanic-notes">
+        <label
+          className="block text-[#E1E1E1] font-redHat text-[13px] mb-2"
+          htmlFor="mechanic-notes"
+        >
           Enter Mechanic Notes
         </label>
         <input
           id="mechanic-notes"
           type="text"
-          className="w-full p-2 border border-gray-400 rounded-full"
+          className="w-full p-2 border placeholder:text-black font-redHat text-[13px] border-gray-400 rounded-full"
           placeholder="Share your initial pre-check and what you’d like to check first"
           value={mechanicNotes}
           onChange={(e) => setMechanicNotes(e.target.value)}
         />
       </div>
       <div className="mb-6">
-        <label className="block text-white mb-2" htmlFor="active-dtcs">
+        <label className="block text-[#E1E1E1] font-redHat text-[13px] mb-2">
           Active DTCs
         </label>
         <input
           id="active-dtcs"
           type="text"
-          className="w-full p-2 border border-gray-400 rounded-full"
+          className="w-full p-2 border placeholder:text-black font-redHat text-[13px] border-gray-400 rounded-full"
           placeholder="List all active DTCs activate on the vehicle"
           value={activeDTCs}
           onChange={(e) => setActiveDTCs(e.target.value)}
@@ -63,13 +135,13 @@ const CustomerIntakeForm = () => {
       </div>
       <div className="flex justify-between gap-5">
         <button
-          className="px-4 py-2 bg-white w-6/12 text-gray-700 border border-gray-400 rounded-full hover:bg-gray-100"
+          className="px-4 py-2 bg-white w-6/12 font-redHat text-[13px] text-gray-700 border border-gray-400 rounded-full hover:bg-gray-100"
           onClick={handleCancel}
         >
           Cancel
         </button>
         <button
-          className="px-4 py-2 bg-black w-6/12 text-white rounded-full hover:bg-gray-800"
+          className="px-4 py-2 bg-black w-6/12 font-redHat text-[13px] text-white rounded-full hover:bg-gray-800"
           onClick={handleSave}
         >
           Save
--- src/components/Screen3.tsx
+++ src/components/Screen3.tsx
@@ -1,54 +1,15 @@
-import { Settings, MessageSquare } from "lucide-react";
-
 import CustomerIntakeForm from "./CustomerIntakeForm";
+import Sidebar from "./Sidebar";
 
 export default function Screen3() {
-  const recentCompletedJobs = [
-    { car: "Ford Explorer 2020", issue: "transmission leak" },
-    { car: "Toyota Corolla 2018", issue: "engine oil" },
-    { car: "GMC Sierra 2015", issue: "brake fluid" },
-    { car: "Toyota Prius 2023", issue: "change tires" },
-    { car: "Ford Fusion 2015", issue: "electrical wiring" },
-  ];
   return (
     <div className="flex  bg-gray-100">
-      <div className="w-1/4 bg-black/60 p-4 flex min-h-screen flex-col">
-        <button className="bg-black text-white py-2 px-4 rounded mb-4">
-          Diagnose New Vehicle
-        </button>
-        <div className="flex justify-between mb-4">
-          <button className="flex items-center">
-            <MessageSquare className="mr-2" />
-            Quick Chat
-          </button>
-          <button className="flex items-center">
-            <Settings className="mr-2" />
-            Settings
-          </button>
-        </div>
-        <div className="h-4/6 bg-white  p-5 rounded-lg  overflow-y-auto">
-          <h3 className="font-bold mb-2 text-2xl h-1/6">Recent Vehicles</h3>
-          <section className="flex flex-col justify-between h-4/6">
-            {recentCompletedJobs.map((_job, index) => (
-              <p key={index} className="mb-2 text-base font-medium">
-                Vehicle {index + 1}
-              </p>
-            ))}
-          </section>
-          <p className="text-sm text-blue-500">more vehicles...</p>
-        </div>
-        <button className="bg-transparent border-2 border-white py-2 px-4 rounded mt-4">
-          View FAQ for Carvis
-        </button>
-        <button className="bg-transparent border-2 border-white py-2 px-4 rounded mt-4">
-          Ask other mechanics (Link to WhatsApp)
-        </button>
-      </div>
+      <Sidebar />
       <div className="p-10 w-3/4">
         <section className="flex gap-2  w-full border-b-4 pb-4">
           <img src={"../../public/car17.jpg"} alt="" />
-          <section>
-            <p className="text-2xl font-bold">BMW M3 2020</p>
+          <section className="font-header">
+            <p className="text-2xl font-body font-bold">BMW M3 2020</p>
             <p className="font-medium text-black/60">Miles: 50,000</p>
             <p className="font-medium text-black/60">
               Last Service Date: 05/2024
@@ -58,14 +19,40 @@ export default function Screen3() {
         </section>
         <section className="flex flex-col justify-center items-center mt-10">
           <CustomerIntakeForm />
-          <div className="mt-16 w-full">
+          <div className="relative mt-16 w-full">
             <input
               type="text"
               name=""
               id=""
               placeholder="Type your message here..."
-              className="outline-none w-full border-[12px] p-2 border-black/30"
+              className="outline-none w-full border-[12px] font-header text-[14px] p-2 pr-10 border-[#D3D3D3]"
             />
+            <button className="absolute right-3 top-1/2 transform -translate-y-1/2 p-1  rounded-full">
+              <svg
+                width="32"
+                height="31"
+                viewBox="0 0 32 31"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g clip-path="url(#clip0_5_659)">
+                  <path
+                    d="M8.54712 26.1304L10.8334 28.4166L23.75 15.5L10.8334 2.58331L8.54712 4.86956L19.1775 15.5L8.54712 26.1304Z"
+                    fill="#030303"
+                  />
+                </g>
+                <defs>
+                  <clipPath id="clip0_5_659">
+                    <rect
+                      width="31"
+                      height="31"
+                      fill="white"
+                      transform="translate(0.5)"
+                    />
+                  </clipPath>
+                </defs>
+              </svg>
+            </button>
           </div>
         </section>
       </div>
