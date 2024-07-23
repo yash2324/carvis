5a1ed17cbf0ff396e7568b1067377dc23533a343
yash2324 <yg232004@gmail.com>
2024-07-23 22:13:31 +0530

    Vehicle info page made responsive

--- src/components/VehicleInfo.tsx
+++ src/components/VehicleInfo.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import CarBg from "../assets/CarFormBg.png";
+
 interface InputProps {
   type: string;
   placeholder: string;
@@ -7,7 +8,7 @@ interface InputProps {
 
 const Input: React.FC<InputProps> = ({ type, placeholder }) => (
   <input
-    className="p-2 rounded-full w-full placeholder-black font-redHat focus:outline-none focus:ring-2 focus:ring-blue-300 "
+    className="p-2 rounded-full w-full placeholder-black font-redHat focus:outline-none focus:ring-2 focus:ring-blue-300"
     type={type}
     placeholder={placeholder}
   />
@@ -15,23 +16,27 @@ const Input: React.FC<InputProps> = ({ type, placeholder }) => (
 
 const VehicleInfo = () => {
   return (
-    <div className="w-[76%] min-h-screen flex items-center">
-      <img src={CarBg} className="z-0 absolute h-full right-0 top-0 w-[76%]" />
-      <div className="max-w-md w-[24rem] mx-auto border-white border-[1px] z-10 bg-[#7F7F7F] p-3 rounded-md shadow-lg">
+    <div className="w-full lg:w-[76%] min-h-screen flex items-center justify-center relative">
+      <img
+        src={CarBg}
+        className=" lg:block z-0 absolute h-full right-0 top-0 w-full lg:w-full object-cover"
+        alt="Car background"
+      />
+      <div className="w-full max-w-md mx-4 lg:mx-auto border-white border-[1px] z-10 bg-[#7F7F7F] p-3 rounded-md shadow-lg">
         <h2 className="text-xl font-bold font-redHat mb-4 text-white">
           Customer Info
         </h2>
-        <div className="grid grid-cols-2 gap-x-16 gap-y-2 mb-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-4 sm:gap-x-16 gap-y-2 mb-4">
           <Input type="text" placeholder="Name *" />
           <Input type="text" placeholder="Date *" />
           <Input type="tel" placeholder="Phone # *" />
           <Input type="email" placeholder="Email *" />
         </div>
 
-        <h2 className="text-xl font-bold font-redHat  mb-4 text-white">
+        <h2 className="text-xl font-bold font-redHat mb-4 text-white">
           Vehicle Info
         </h2>
-        <div className="grid grid-cols-2 gap-x-16 gap-y-2 mb-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-4 sm:gap-x-16 gap-y-2 mb-4">
           <Input type="text" placeholder="Make *" />
           <Input type="text" placeholder="Model *" />
           <Input type="text" placeholder="Year *" />
